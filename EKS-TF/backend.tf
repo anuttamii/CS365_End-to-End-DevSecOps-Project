94e75ffa7d7d02396c755ef9d0f6ddbed2a19607
anuttamii <anuttama.sae@dome.tu.ac.th>
2026-03-29 05:47:16 +0700

    change s3 to local

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,11 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket       = "tetris-terraform-state-cs366"
-    region       = "us-east-1"
-    key          = "eks/terraform.tfstate"
-    use_lockfile = true
-    encrypt      = true
-  }
+    
   required_version = ">=1.14.0"
   required_providers {
     aws = {
