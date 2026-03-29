56577caf4d19808c3aa34caec33220b62e8c14e4
anuttamii <anuttama.sae@dome.tu.ac.th>
2026-03-29 09:09:15 +0700

    iam-fixed

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "eks-cluster" {
   name     = var.cluster-name
-  role_arn = aws_iam_role.EKSClusterRole.arn
+  role_arn = "arn:aws:iam::333732092862:role/LabRole"
+
   vpc_config {
     subnet_ids         = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
     security_group_ids = [data.aws_security_group.sg-default.id]
@@ -8,5 +9,4 @@ resource "aws_eks_cluster" "eks-cluster" {
 
   version = 1.33
 
-  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
 }
