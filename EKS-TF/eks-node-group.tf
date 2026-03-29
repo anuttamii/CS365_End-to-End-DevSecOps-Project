989bbbf4374906f7193405483d30c4ce90743d8d
anuttamii <anuttama.sae@dome.tu.ac.th>
2026-03-29 09:44:22 +0700

    fix: use labrole only

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -1,9 +1,13 @@
 resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = var.eksnode-group-name
-  node_role_arn = "arn:aws:iam::333732092862:role/LabRole"
-  subnet_ids      = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
 
+  node_role_arn = "arn:aws:iam::333732092862:role/LabRole"
+
+  subnet_ids = [
+    data.aws_subnet.subnet.id,
+    aws_subnet.public-subnet2.id
+  ]
 
   scaling_config {
     desired_size = 2
