e14dae82a794b6b2c77414ead7e771450af7c6f0
anuttamii <anuttama.sae@dome.tu.ac.th>
2026-04-01 19:15:06 +0700

    error fixing

--- EKS-TF/variables.tf
+++ EKS-TF/variables.tf
@@ -1,30 +1,26 @@
-variable "vpc_name" {}
-variable "igw_name" {}
-variable "rt_name2" { type = string }
-variable "subnet_name" {}
-variable "subnet_name2" { type = string }
-variable "security_group_name" {}
-variable "iam_role_eks" {}
-variable "iam_role_node" {}
-variable "iam_policy_eks" {}
-variable "iam_policy_node" {}
 variable "cluster_name" {}
 variable "eksnode_group_name" {}
-
-variable "subnet_id_1" {
-  description = "Subnet ID from AWS"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "Security Group ID from AWS"
-  type        = string
-}
 
 variable "vpc_id" {
   type = string
 }
 
 variable "igw_id" {
+  type = string
+}
+
+variable "subnet_id_1" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "subnet_name2" {
+  type = string
+}
+
+variable "rt_name2" {
   type = string
 }
