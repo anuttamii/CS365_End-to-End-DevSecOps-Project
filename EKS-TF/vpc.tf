7f5123ea5eed1336b34751f89043fc0464bb3eb2
anuttamii <anuttama.sae@dome.tu.ac.th>
2026-04-19 13:54:56 +0700

    cidr edited

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -3,7 +3,7 @@
 # ========================
 resource "aws_subnet" "public_subnet2" {
   vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.5.0/24"  
+  cidr_block              = "10.0.6.0/24"  
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
